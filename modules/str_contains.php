a47358ed9485f6e555dd4c4bbc8279f1d6ec0f20
elfasi_shir <torushojiro@gmail.com>
2025-08-11 21:10:03 +0300

    just spacing

--- modules/str_contains.php
+++ modules/str_contains.php
@@ -1,9 +1,8 @@
 <?php
 
-	if (!function_exists('str_contains')) {
-		function str_contains(string $haystack, string $needle): bool {
-			return $needle !== '' && strpos($haystack, $needle) !== false;
-		}
+if (!function_exists('str_contains')) {
+	function str_contains(string $haystack, string $needle): bool
+	{
+		return $needle !== '' && strpos($haystack, $needle) !== false;
 	}
-	
-?>
+}
